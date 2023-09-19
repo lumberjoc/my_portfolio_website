900678f7f4e218366572ca740d2071156a8d7458
lumberjoc <132397996+lumberjoc@users.noreply.github.com>
2023-09-19 09:26:10 -0700

    update: create intro
    - setup text and styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,6 +12,13 @@ const Hero = () => {
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
+        
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Jacques</span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+          From DevOps to Development, <br className="sm:block hidden" />I create whatever, whenever, and wherever. 
+          </p>
+        </div>
       </div>
     </section>
   )
