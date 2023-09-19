bac2a04e66a792a66c8947355cc7aa2c23dda505
lumberjoc <132397996+lumberjoc@users.noreply.github.com>
2023-09-19 07:50:17 -0700

    feature: menu
    - add functionality to remove the list and replace with menu bar
    - menu bar appears after clicking

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,9 @@ import { logo, menu, close } from '../assets';
 
 
 const Navbar = () => {
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
+
   return (
    <nav
     className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
@@ -21,7 +24,36 @@ const Navbar = () => {
         }}
       >  
         <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+        <p className="text-white text-[18px] font-bold cursor-pointer">Jacques <span className="sm:block-hidden"> Beauvoir</span> </p>
       </Link>
+      <ul className="list-none hidden sm:flex flex-row gap-10">
+        {navLinks.map((link) => (
+          <li 
+            key={link.id}
+            className={`${
+              active === link.title
+              ? "text-white"
+              : "text-secondary"
+            } hover:text-white text-[18px] font-medium cursor-pointer`}
+            onClick={() => setActive(link.title)}
+          >
+            <a href={`#${link.id}`}>{link.title}</a>
+          </li>
+        ))}
+      </ul>
+      {/* the sm:hidden below enables the menu bar to appear on small devices */}
+      <div className="sm:hidden flex flex-1 justify-end items-center"> 
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain"
+          onClick={() => setToggle(!toggle)} // boolean dictating whether or not to show the pancakes
+        />
+        {/* Shows the actual menu with a dynamic template string */}
+        <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-x1`}>
+
+        </div>
+      </div>
     </div>
 
    </nav>
