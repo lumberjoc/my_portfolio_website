f15ce5d8052c963e409dbaf461770c6e45184662
lumberjoc <132397996+lumberjoc@users.noreply.github.com>
2023-09-19 10:38:26 -0700

    new component: ComputerCanvas
    - creates model to load into primitive
    - had to install three (npm install --legacy-peer-deps three)
    - issue with div element contained within the canvas loader component
    (https://discourse.threejs.org/t/uncaught-error-r3f-div-is-not-part-of-the-three-namespace-did-you-forget-to-extend/48783)

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,4 +1,4 @@
-import { Suspense, useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber"; // provides canvas to place on
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei"; // helpers to allow us draw and import 3d models 
 
@@ -17,4 +17,28 @@ const Computers = () => {
   )
 }
 
-export default Computers
+// Load in
+const ComputerCanvas = () => {
+  return (
+    // React component to enable loader while model is loading
+    <Suspense fallback={<CanvasLoader />}> 
+      <Canvas
+        frameloop='demand'
+        shadows
+        camera={{ position: [20, 3, 5], fov: 25 }}
+        gl={{ preserveDrawingBuffer: true }} // used to properly render model
+      >
+          <OrbitControls // enables movement of model from left to right 
+            enableZoom={false}
+            maxPolarAngle={Math.PI / 2}
+            minPolarAngle={Math.PI / 2}
+          />
+          <Computers /> 
+        <Preload all />
+      </Canvas>
+    </Suspense>
+
+  )
+}
+
+export default ComputerCanvas
