b98048b3577c1a2f773bc0ccf3b59c0f7e4ce5d0
lumberjoc <132397996+lumberjoc@users.noreply.github.com>
2023-09-19 09:47:48 -0700

    initial setup

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber"; // provides canvas to place on
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei"; // helpers to allow us draw and import 3d models 
+
+import CanvasLoader from '../Loader';
 
 const Computers = () => {
+  const computer = useGLTF('./desktop_pc/scene.gltf');
   return (
-    <div>Computers</div>
+
   )
 }
 
