918857cfc461931201d159c27407be495254eaba
lumberjoc <132397996+lumberjoc@users.noreply.github.com>
2023-09-20 16:58:06 -0700

    feature: adding job title, company name, dates

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -8,8 +8,29 @@ import { experiences  } from '../constants';
 import { SectionWrapper } from '../hoc';
 import { textVariant } from "../utils/motion";
 
-
+const ExperienceCard = ({ experience }) => (
+      <VerticalTimelineElement
+        contentStyle={{ background: '#1d186', color:'#fff'}}
+        contentArrowStyle={{ borderRight: '7px solid #232631'}}
+        date={experience.date}
+        iconStyle={{background: experience.iconBg }}
+        icon={
+          <div className="flex justify-center items-center w-full h-full">
+            <img 
+              src={experience.icon}
+              alt={experience.company_name}
+              className="w-[60%] h-[60%] object-contain"
+            />
+          </div>
+        }
+      >
+        <div>
+          <h3 className="text-white text-[24px] font-bold">{experience.title}</h3>
+          <p className="text-secondary text-[16px] font-semibold" style={{margin: 0}}>
+            {experience.company_name}
+          </p>
         </div>
+
       </VerticalTimelineElement>
 )
  
