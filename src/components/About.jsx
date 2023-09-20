195b7f15ef3dbd2072157c2624ed59d0937495b0
lumberjoc <132397996+lumberjoc@users.noreply.github.com>
2023-09-19 22:42:23 -0700

    initial setup
    - create a ServiceCard functional component to display titles
    - create logic to loop through the services

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,16 @@
 import React from 'react'
+import { Tilt } from "react-tilt";
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <p>{title}</p>
+  )
+}
 
 const About = () => {
   return (
--- src/constants/index.js
+++ src/constants/index.js
@@ -46,15 +46,15 @@ import {
       icon: web,
     },
     {
-      title: "React Native Developer",
+      title: "React Developer",
       icon: mobile,
     },
     {
-      title: "Backend Developer",
+      title: "Devops",
       icon: backend,
     },
     {
-      title: "Content Creator",
+      title: "Site Reliability",
       icon: creator,
     },
   ];
