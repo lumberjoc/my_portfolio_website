5db044616e39551564b6f53c820b3a72e5640e1f
lumberjoc <132397996+lumberjoc@users.noreply.github.com>
2023-09-20 15:48:44 -0700

    adding motion + wrapping

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -11,8 +11,14 @@ import { textVariant } from "../utils/motion";
 
 const Experience = () => {
   return (
-    <div>Experience</div>
+    <>
+      {/* variants enables nested tags within to animate */}
+      <motion.div variants={textVariant()}> 
+        <p className={styles.sectionSubText}>What I have done so far</p>
+        <h2 className={styles.sectionHeadText}>Work Experience</h2>
+      </motion.div>
+    </>
   )
 }
 
-export default Experience
+export default SectionWrapper(Experience, "experience")
