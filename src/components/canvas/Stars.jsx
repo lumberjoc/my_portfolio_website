2fe9bbd882647ccb98b8104e4589ae6be5aea7e8
lumberjoc <132397996+lumberjoc@users.noreply.github.com>
2023-09-25 16:47:32 -0700

    create canvas for stars

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -13,7 +13,13 @@ const Stars = (props) => {
 const StarsCanvas = () => {
   return (
     <div className="w-full h-full absolute inset-0 z-[-1]">
+      <Canvas>
+        <Suspense fallback={null}>
+          <Stars />
+        </Suspense>
 
+        <Preload all />
+      </Canvas>
     </div>
   )
 }
