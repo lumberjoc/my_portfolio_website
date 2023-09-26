9d1b2a1811300d710bea8aaa1b82aa6c653111bf
lumberjoc <132397996+lumberjoc@users.noreply.github.com>
2023-09-25 17:09:40 -0700

    create motion for stars

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -8,6 +8,12 @@ const Stars = (props) => {
   const ref = useRef();
   const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2})
 
+  // Allows us to rotate stars frame by frame
+  useFrame((state, delta) => {
+    ref.current.rotation.x -= delta / 10;
+    ref.current.rotation.y -= delta / 15;
+  });
+
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
       <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
